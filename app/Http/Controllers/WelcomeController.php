9b20f2c29cb3d45017c6c960152c59ebb70c5841
Albert Chen <youcan689@gmail.com>
2021-02-24 16:49:43 +0800

    預期要看到WelcomeController.php內下方view，但是語法有顯示錯誤，看了官方文件後官方文件view標籤內寫的是有帶參數，但是我這個view沒有要帶參數，除錯需要指引

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,6 +9,7 @@ class WelcomeController extends Controller
     //
 
     function about(){
+        //預期要看到下方view，但是下方有顯示錯誤，看了官方文件後官方文件view標籤內寫的是有帶參數，但是我這個view沒有要帶參數
         return view(view:"about");
       }
       
