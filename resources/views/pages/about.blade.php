813424b6f8dd7572bcf92f0f9c4f71faf09729af
Albert Chen <youcan689@gmail.com>
2021-02-25 18:36:06 +0800

    Day7 layout title(失敗)

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,23 +1,26 @@
 @extends('layouts/application')
+@section('main')
+    <h1>hi</h1>
+    @if(count($heros)>0)
+
+        @foreach($heros as $hero)
+            @if($loop->first || $loop->last)
+            <li>
+                <strong>{{$hero}}</strong>
+            </li>
+            @else
+            <li>
+                {{$hero}}   
+            </li>
+            @endif
+        @endforeach
 
 
-<h1>hi</h1>
-@if(count($heros)>0)
 
-    @foreach($heros as $hero)
-        @if($loop->first || $loop->last)
-        <li>
-            <strong>{{$hero}}</strong>
-        </li>
-        @else
-        <li>
-            {{$hero}}   
-        </li>
-        @endif
-    @endforeach
+    @endif
+
+@endsection
 
 
-
-@endif
-
+ @section('title','程式累積腳踏實地')
  
